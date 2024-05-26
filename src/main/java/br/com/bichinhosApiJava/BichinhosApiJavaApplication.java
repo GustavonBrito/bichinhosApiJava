c607f26852159bbabee8c20eaa1d432da619dd38
GustavonBrito <gustavoserafio@gmail.com>
2024-05-26 18:31:42 -0300

    Initial commit

---
+++ src/main/java/br/com/bichinhosApiJava/BichinhosApiJavaApplication.java
@@ -0,0 +1,13 @@
+package br.com.bichinhosApiJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BichinhosApiJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BichinhosApiJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/bichinhosApiJava/BichinhosApiJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bichinhosApiJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BichinhosApiJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
